96994b189c4b7a6688dd2e8e044879216b1af9df
EbertPalmeira <eberttpalmerass@gmail.com>
2024-08-16 15:40:09 -0300

    alteracao

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import './App.css'
 import Header from './Components/Header'
 import Main from './Components/Main'
 
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+
 function App() {
- 
+  const theme = createTheme({
+    typography: {
+      fontFamily: '"Poppins", sans-serif', 
+      
+      
+    },
+  });
 
   return (
     <Grid sx={{background:'black',margin:'0'}}>
+      <ThemeProvider theme={theme}>
       <Header />
       <Main/>
+      </ThemeProvider>
     </Grid>
   )
 }
--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  {Box, Grid}  from '@mui/material'
+import  {Box, Button, Grid,Typography}  from '@mui/material'
 import code from '../../imgs/code.svg'
 import { FaLinkedin,FaGithub } from "react-icons/fa";
 import './styles.css'
@@ -10,29 +10,90 @@ const Main = () => {
   return (
     <Grid container spacing={2} sx={{display:'flex',justifyContent:'center',marginTop:'50px',color:'#fff',alignItems:'center',gap:'200px'}}>
 
-        <Grid xs={12} sm={6} md={4} lg={3} sx={{alignItems:'center'}}>
-            <Grid  sx={{marginBottom:'20px'}}>
-                <p style={{fontSize:'20px',marginBottom:'10px'}}>Olá, eu sou</p>
-                <h2 style={{fontSize:'2.5em',marginBottom:'10px'}}>Ebert Palmeira</h2>
-                <p style={{fontSize:'20px',marginBottom:'10px'}}> Front-end Developer</p>
+        <Grid  sx={{alignItems:'center'}}>
+            <Grid className='sobre' sx={{marginBottom:'20px'}}>
+                <Typography 
+                    sx={{fontSize:'20px',
+                        marginBottom:'10px',
+                        fontWeight:'600'
+                    }}>
+                    Olá, eu sou
+                    </Typography>
+                
+                <Typography 
+                    sx={{fontSize:'2.5em',
+                    marginBottom:'10px',
+                    fontWeight:'600',
+                    '@media (max-width:600px)': {
+                    fontSize: '2.1em', 
+                    },
+
+                }}>
+                    Ebert Palmeira
+                </Typography>
+                <Typography 
+                    sx={{
+                        fontSize:'20px',
+                        marginBottom:'10px',
+                        fontWeight:'600'
+                        }}> 
+                        Front-end Developer
+                        </Typography>
             </Grid>
-            <Grid xs={12} sm={6} md={4} lg={3} className='btn' >
-                <button className='link'>
+            <Grid  xs={12} sm={6} md={4} lg={3} sx={{
+                display:"grid",
+                textAlign:'center',
+                alignItems:'center',
+                gap: '20px',
+                
+                }} className='btn' >
+                <Button sx={{
+                        display:'flex',
+                        alignItems:'center',
+                        justifyContent:'center',
+                        background: 'linear-gradient(to right, #120f2b, #6a97ff);',
+                        border:' 1px solid #6a97ff',
+                        transition:'0.5s',
+                        width: '300px',
+                        height:'70px',
+                        gap: '10px',
+                        color:'#fff',
+
+                        '@media (max-width:400px)':{
+                        width: '200px',
+                        height:' 70px',
+                        
+                    }
+                    
+                }} className='link'>
                     <FaLinkedin size={30} style={{background:'none',color:'#fff'}}/>
                    
-                    <a href='' style={{background:'none',}}> LinkedIn</a>
-                    </button>
+                    <a href='' style={{background:'none',color:'#fff'}}> LinkedIn</a>
+                </Button>
 
-                    <button  className='git'>
+                    <Button sx={{
+                        background: 'linear-gradient(to right, #17152e, #8a2be2)',
+                        border:' 1px solid #8a2be2',
+                        transition:' 0.5s',
+                        width: '300px',
+                        height:'70px',
+                        gap: '10px',
+
+                        '@media (max-width:400px)':{
+                        width: '200px',
+                        height:' 70px',
+
+                        }
+                    }} className='git'>
                     <FaGithub size={30} style={{background:'none',color:'#fff'}}/>
                    
-                    <a href='' style={{background:'none'}}>GitHub</a>
-                    </button>
+                    <a href='' style={{background:'none',color:'#fff'}}>GitHub</a>
+                    </Button>
             </Grid>
 
         </Grid>
-        <Box>
-            <img style={{width:'550px'}} src={code} alt="" />
+        <Box className="imgPc">
+            <img  src={code} alt="" />
         </Box>
 
 
